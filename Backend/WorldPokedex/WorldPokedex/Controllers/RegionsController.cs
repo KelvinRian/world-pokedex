2b06f832cb6d0f99d07823f33aaa31c3bacc9022
Kelvin Rian Lourenco <kelvin,lourenco18@gmail.com>
2025-04-12 14:03:02 -0300

    add basic file upload funcionality

---
+++ Backend/WorldPokedex/WorldPokedex/Controllers/RegionsController.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorldPokedex.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : Controller
+    {
+        private static IWebHostEnvironment _enviroment;
+
+        public RegionsController(IWebHostEnvironment enviroment)
+        {
+            _enviroment = enviroment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task Create([FromForm] IFormFile coverImage)
+        {
+            try
+            {
+                // Se não existe um diretório para as imagens, cria um.
+                if (!Directory.Exists(_enviroment.WebRootPath + "\\images\\"))
+                {
+                    Directory.CreateDirectory(_enviroment.WebRootPath + "\\images\\");
+                }
+
+                // Criamos um arquivo no diretório e guardamos ele na variável fileStream.
+                using (FileStream fileStream = System.IO.File.Create(_enviroment.WebRootPath + "\\images\\" +  coverImage.FileName))
+                {
+                    //CopyToAsync copia o conteúdo de coverImage para o arquivo na variável fileStream
+                    await coverImage.CopyToAsync(fileStream);
+                    fileStream.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
